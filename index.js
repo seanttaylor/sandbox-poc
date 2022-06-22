08ffefeaa7a53bd788ce796657098f15e363a13c
Sean Travis Taylor <sean.travis.taylor@gmail.com>
2022-06-22 16:48:34 +0000

    ADDED `stopAll` METHDO TO SANDBOX CONTROLLER. Added JSDOC blocs

--- index.js
+++ index.js
@@ -3,38 +3,13 @@ import AppEvents from './lib/events/index.js';
 import Ajax from './lib/ajax/index.js';
 import Errors from './lib/errors/index.js';
 import Slideshows from './lib/repos/slideshows/index.js';
+import JainkyModule from './lib/jainky-module/index.js';
 
 Sandbox.module('/lib/errors', Errors);
 Sandbox.module('/lib/events', AppEvents);
 Sandbox.module('/lib/ajax', Ajax);
 Sandbox.module('/lib/repos/slideshows', Slideshows);
-
-Sandbox.module('/lib/jainky-module', (box) => {
-  const { ApplicationError } = box.get('errors');
-  const events = box.get('events');
-
-  box.put('jainky', { getJainky });
-
-  function getJainky() {
-
-  }
-
-  /*const interval = setInterval(()=> {
-    ApplicationError({
-      errorName: 'LibJainkyModuleError', 
-      errorType: 'module.jainky',
-      moduleName: '/lib/jainky-module',
-    }); 
-  }, 3000);
-  */
-
-  /**
-   * 
-   */
-  return function stop() {
-    clearInterval(interval);
-  }
-});
+Sandbox.module('/lib/jainky-module', JainkyModule);
 
 Sandbox.of([
   '/lib/ajax',
@@ -49,14 +24,28 @@ Sandbox.of([
     const slideshow = sb.get('slideshow');
     const data = await fetch({ url: 'https://httpbin.org/json' });
     
-    events.on('application.error', (errorData)=> {
-      ctrl.handleModuleError(errorData, events);
-      console.log(ctrl.getModuleAnalytics());
-    });
-
     events.emit('slideshow.downloaded', data);
+
+    events.on('application.error', onApplicationError);
+
+    /**
+     * 
+     * @param {String} module - a module that exceeded the error threshold, triggering a stop request
+     */
+    function onGlobalModuleErrorThresholdExceeded(module) {
+      events.emit('application.info.moduleStopped', module);
+    }
+
+    /**
+     * @param {AppEvent} appEvent - an instance of the {AppEvent} interface
+     */
+    function onApplicationError(appEvent) {
+      ctrl.handleModuleError(appEvent, onGlobalModuleErrorThresholdExceeded);
+    }
+
     
     setTimeout(()=> {
+      slideshow.create();
       slideshow.create();
       slideshow.create();
     }, 50000)
