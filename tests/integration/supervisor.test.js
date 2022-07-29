c9377aed69358e07143db983d45ca5b39fa748c6
Sean Travis Taylor <sean.travis.taylor@gmail.com>
2022-07-29 16:10:45 +0000

    [WIP] ISSUE NO. 22 Refactored lib/supervisor integration tests.

--- tests/integration/supervisor.test.js
+++ tests/integration/supervisor.test.js
@@ -14,83 +14,116 @@ describe('Supervisor', () => {
         PluginEventAuthz(controller);
         Supervisor(controller);
 
-        sandbox.my.supervisor.setErrorThreshold(47);
-        
-        expect(sandbox.my.supervisor.getErrorThreshold() === 47).toBe(true);
+        sandbox.my.supervisor.setErrorThreshold(42);
+
+        const { AppEvent } = controller.get('/plugins/events-authz');
+
+        sandbox.my.supervisor.onApplicationError(
+            AppEvent({
+                code: 'service.error',
+                message: 'The post could not be created',
+                name: 'LibPostRouterError',
+                module: '/lib/plugins/router/post',
+                _open: {
+                    message: faker.hacker.phrase(),
+                    serviceName: 'postService',
+                }
+            })
+        );
+
+        const myHistogram = sandbox.my.supervisor.getErrors();
+
+        myHistogram.analyze((_histogram) => {
+            expect(_histogram['service.error']['postService'] === 1).toBe(true);
+        });
+
+        expect(sandbox.my.supervisor.getErrorThreshold() === 42).toBe(true);
     });
 
     test('Should be able to update the count of errors when the `application.error` event fires', async () => {
         const sandbox = {};
         const { controller } = SandboxController(sandbox);
-        
+
         PluginEventAuthz(controller);
         Supervisor(controller);
 
-        const events = controller.get('/plugins/events-authz');
+        const { AppEvent } = controller.get('/plugins/events-authz');
+
         // We emit the event twice to exercise both the case this is the first time this event type has been emitted as 
         // well as the case where this event type has been emitted before
-        events.notify('application.error', {
-            code: 'service.error',
-            message: 'The post could not be created',
-            name: 'LibPostRouterError',
-            module: '/lib/plugins/router/post',
-            _open: { 
-                message: faker.hacker.phrase(), 
-                serviceName: 'postService',
-            }
-        });
-        events.notify('application.error', {
-            code: 'service.error',
-            message: 'The post could not be created',
-            name: 'LibPostRouterError',
-            module: '/lib/plugins/router/post',
-            _open: { 
-                message: faker.hacker.phrase(), 
-                serviceName: 'postService',
-            }
-        });
+        sandbox.my.supervisor.onApplicationError(
+            AppEvent({
+                code: 'service.error',
+                message: 'The post could not be created',
+                name: 'LibPostRouterError',
+                module: '/lib/plugins/router/post',
+                _open: {
+                    message: faker.hacker.phrase(),
+                    serviceName: 'postService',
+                }
+            })
+        );
+
+        sandbox.my.supervisor.onApplicationError(
+            AppEvent({
+                code: 'service.error',
+                message: 'The post could not be created',
+                name: 'LibPostRouterError',
+                module: '/lib/plugins/router/post',
+                _open: {
+                    message: faker.hacker.phrase(),
+                    serviceName: 'postService',
+                }
+            })
+        );
 
         const myHistogram = sandbox.my.supervisor.getErrors();
         myHistogram.analyze((_histogram) => {
             expect(_histogram['service.error']['postService'] === 2).toBe(true);
-        });        
+        });
     });
 
     test('Should be able to update the count of errors when the `application.error` event fires', async () => {
         const sandbox = {};
         const { controller } = SandboxController(sandbox);
-        
+
         PluginEventAuthz(controller);
         Supervisor(controller);
 
-        const events = controller.get('/plugins/events-authz');
+        const { AppEvent } = controller.get('/plugins/events-authz');
+    
         sandbox.my.supervisor.setErrorThreshold(1);
 
         // We emit the event twice to ensure the `application.error.globalErrorThreshholdExceeded` event fires
-        events.notify('application.error', {
-            code: 'service.error',
-            message: 'The post could not be created',
-            name: 'LibPostRouterError',
-            module: '/lib/plugins/router/post',
-            _open: { 
-                message: faker.hacker.phrase(), 
-                serviceName: 'postService',
-            }
-        });
-        events.notify('application.error', {
-            code: 'service.error',
-            message: 'The post could not be created',
-            name: 'LibPostRouterError',
-            module: '/lib/plugins/router/post',
-            _open: { 
-                message: faker.hacker.phrase(), 
-                serviceName: 'postService',
-            }
-        });
+        sandbox.my.supervisor.onApplicationError(
+            AppEvent({
+                code: 'service.error',
+                message: 'The post could not be created',
+                name: 'LibPostRouterError',
+                module: '/lib/plugins/router/post',
+                _open: {
+                    message: faker.hacker.phrase(),
+                    serviceName: 'postService',
+                }
+            })
+        );
+
+        sandbox.my.supervisor.onApplicationError(
+            AppEvent({
+                code: 'service.error',
+                message: 'The post could not be created',
+                name: 'LibPostRouterError',
+                module: '/lib/plugins/router/post',
+                _open: {
+                    message: faker.hacker.phrase(),
+                    serviceName: 'postService',
+                }
+            })
+        );
 
         const myHistogram = sandbox.my.supervisor.getErrors();
         myHistogram.analyze((_histogram) => {
             expect(_histogram['service.error']['postService'] === 2).toBe(true);
-        });        
+        });
     });
 });
